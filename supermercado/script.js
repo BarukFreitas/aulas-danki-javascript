4842e8aebf91c322dd802b4ffe14c0b4758cad38
Baruk Freitas <baruk.freitas@gmail.com>
2024-01-09 00:07:07 -0300

    Update script.js

---
+++ supermercado/script.js
@@ -0,0 +1,9 @@
+var itens = [];
+
+document.querySelector('input[type=submit]')
+.addEventListener('click',()=>{
+    var nome_produto = document.querySelector('input[name="valor_produto]"').value
+
+    alert(nome_produto)
+
+});
